365b179d3a00eac80eae42fb74b64be63278c971
MuzanChiquito <cris.morrizz@gmail.com>
2024-11-23 16:54:19 -0600

    Se agraga el Programa

---
+++ Tapete $$/Tapete $$/MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: crist
+ * Date: 13/11/2024
+ * Time: 07:27 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Tapete___
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label8;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(35, 88);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(175, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Costo de bulto de aserrin:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(216, 85);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(159, 22);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(35, 111);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(133, 25);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Costo L de pintura:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(174, 108);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(201, 22);
+			this.textBox2.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(35, 136);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(189, 23);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Costo de flores por docena:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(230, 133);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(145, 22);
+			this.textBox3.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(35, 159);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(148, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Número de personas:";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(189, 156);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(186, 22);
+			this.textBox4.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(35, 182);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(105, 23);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Litro de agua:";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(146, 179);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(229, 22);
+			this.textBox5.TabIndex = 11;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(35, 205);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(241, 23);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "Costo por hora de tiempo de diseño:";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(282, 202);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(93, 22);
+			this.textBox6.TabIndex = 13;
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(35, 228);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(241, 23);
+			this.label7.TabIndex = 14;
+			this.label7.Text = "Costo de mano de obra por persona:";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(282, 225);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(93, 22);
+			this.textBox7.TabIndex = 15;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(81, 280);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(87, 23);
+			this.button1.TabIndex = 18;
+			this.button1.Text = "Resultado";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(216, 280);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(87, 23);
+			this.label8.TabIndex = 19;
+			this.label8.Text = " ";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(424, 408);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Tapete $$";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Tapete $$/Tapete $$/MainForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: crist
+ * Date: 13/11/2024
+ * Time: 07:27 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapete___
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+		void Button1Click(object sender, EventArgs e)
+		{
+			int CoA, CoLP, CoFD, NoP, CoLA, CoHD, CoMO, Total;
+			CoA = int.Parse(textBox1.Text);
+			CoLP = int.Parse(textBox2.Text);
+			CoFD =  int.Parse(textBox3.Text);
+			NoP =  int.Parse(textBox4.Text);
+			CoLA =  int.Parse(textBox5.Text);
+			CoHD =  int.Parse(textBox6.Text);
+			CoMO =  int.Parse(textBox7.Text);
+			
+			CoA = CoA * 100;
+			CoLP = CoLP * 239;
+			CoFD = CoFD * 136;
+			NoP = NoP * 200;
+			CoLA = CoLA * 13;
+			CoHD = CoHD * 200;
+			Total = CoA + CoLP + CoFD + NoP + CoLA + CoHD + CoMO;
+			label8.Text = Total.ToString();
+		}
+		
+	}
+}
